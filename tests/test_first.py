0ccb4d9e57b57d6e4c3513df784cbf3a24beb3ab
nunininu <sgcho0907@gmail.com>
2025-01-14 17:28:19 +0900

    수정중

--- tests/test_first.py
+++ tests/test_first.py
@@ -3,7 +3,7 @@ from nunininu_check_os_ver.osver import get_os_pretty_name
 def test_first():
     v = get_os_pretty_name()
     assert v is not None
-    assert v == "ubuntu 24.04.1 LTS"
+    assert v == "Ubuntu 24.04.1 LTS"
     # 빈 문자열이 아닌지지
     # 문자열에 LTS가 포함되었는지
     # 문자열에 문자도 있고, 숫자도 있는지
