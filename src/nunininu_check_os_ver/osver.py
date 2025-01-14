fd1a9cacc09600a0f43c9944263428d9256801ca
nunininu <sgcho0907@gmail.com>
2025-01-14 17:13:42 +0900

    pytest 내용 작성중

--- src/nunininu_check_os_ver/osver.py
+++ src/nunininu_check_os_ver/osver.py
@@ -3,7 +3,6 @@ def get_os_pretty_name(): -> str:
     with open('/etc/os-release', 'r') as file:
         for line in file:
             if line.startswith('PRETTY_NAME'):
-                 PRETTY_NAME=\"Ubuntu 24.0.1 LTS\"\n"
-                 \"Ubuntu 24.0.1 LTS\"\n"
+                 # \"Ubuntu 24.0.1 LTS\"\n"
                 return line.split('=')[1].replace('\n','').strip("\"")
     return None
